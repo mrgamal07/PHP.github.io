e7696cc9a43c157c154731995e7b1cb4572e240a
mrgamal07 <gamalarun@gmail.com>
2025-11-18 07:35:19 +0545

    init

---
+++ abc.php
@@ -0,0 +1,52 @@
+<?php
+date_default_timezone_set('Asia/Kathmandu'); // Set timezone to Kathmandu
+
+echo "Day Formats:<br>";
+echo "d: " . date("d") . "<br>";
+echo "D: " . date("D") . "<br>";
+echo "j: " . date("j") . "<br>";
+echo "l: " . date("l") . "<br>";
+echo "N: " . date("N") . "<br>";
+echo "S: " . date("S") . "<br>";
+echo "w: " . date("w") . "<br>";
+echo "z: " . date("z") . "<br><br>";
+
+echo "Week & Month Formats:<br>";
+echo "W: " . date("W") . "<br>";
+echo "F: " . date("F") . "<br>";
+echo "m: " . date("m") . "<br>";
+echo "M: " . date("M") . "<br>";
+echo "n: " . date("n") . "<br>";
+echo "t: " . date("t") . "<br><br>";
+
+echo "Year Formats:<br>";
+echo "L: " . date("L") . "<br>";
+echo "o: " . date("o") . "<br>";
+echo "Y: " . date("Y") . "<br>";
+echo "y: " . date("y") . "<br><br>";
+
+echo "Time Formats:<br>";
+echo "a: " . date("a") . "<br>";
+echo "A: " . date("A") . "<br>";
+echo "B: " . date("B") . "<br>";
+echo "g: " . date("g") . "<br>";
+echo "G: " . date("G") . "<br>";
+echo "h: " . date("h") . "<br>";
+echo "H: " . date("H") . "<br>";
+echo "i: " . date("i") . "<br>";
+echo "s: " . date("s") . "<br>";
+echo "u: " . date("u") . "<br><br>";
+
+echo "Timezone Formats:<br>";
+echo "e: " . date("e") . "<br>";
+echo "I: " . date("I") . "<br>";
+echo "O: " . date("O") . "<br>";
+echo "P: " . date("P") . "<br>";
+echo "T: " . date("T") . "<br>";
+echo "Z: " . date("Z") . "<br><br>";
+
+echo "Full Date/Time Formats:<br>";
+echo "c: " . date("c") . "<br>";
+echo "r: " . date("r") . "<br>";
+echo "U: " . date("U") . "<br>";
+?>
